54649a088f26a461aa64617c0eae264bbf7708d6
subham <58190278+onepieceof-code@users.noreply.github.com>
2026-06-17 09:09:53 +0530

    lean and added of TS and css

--- app/resturent/src/pages/home/home.tsx
+++ app/resturent/src/pages/home/home.tsx
@@ -1,29 +1,38 @@
 import { useState } from "react";
 
+import "./home.css";
+
 import {
-  Page, Bar, Title, Form,
+  Page,
+  Bar,
+  Title,
+  Form,
   FormGroup,
   FormItem,
   Input,
   Select,
   Option,
-  Card
+  Card,
 } from "@ui5/webcomponents-react";
 
 type BasicDetails = {
-  name: string,
-  email: string,
-  number: string
+  name: string;
+  email: string;
+  number: string;
 };
 
 type AddressDetails = {
-  addressLine1: string,
-  addressLine2: string,
-  city: string,
-  stateProvince: string,
-  postalCode: BigInteger,
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  stateProvince: string;
+  postalCode: string;
 };
 
+interface AddressBoxProps {
+  addressDetails: AddressDetails;
+  setAddressDetails: React.Dispatch<React.SetStateAction<AddressDetails>>;
+}
 
 export default function Home() {
   // useEffect(()=>{
@@ -31,71 +40,81 @@ export default function Home() {
   // })
 
   return (
-
-    <div style={{ height: "100vh" }}>
-      <Page header={
-
-        <Bar>
-          <Title>Register</Title>
-        </Bar>
-
-      }
-
+    <div className="MainContainer">
+      <Page
+        header={
+          <Bar>
+            <Title>Register</Title>
+          </Bar>
+        }
       >
         <Register />
       </Page>
     </div>
-
-  )
+  );
 }
 
-
 function Register() {
-
   const [showAddress, setShowAddress] = useState<boolean>(true);
 
-
-  const [basicDetails, setBasicDetails] = useState<BasicDetails>({ name: "", email: "", number: "" });
-  const [addressDetails, setaddressDetails] = useState<AddressDetails>({
+  const [basicDetails, setBasicDetails] = useState<BasicDetails>({
+    name: "",
+    email: "",
+    number: "",
+  });
+  const [addressDetails, setAddressDetails] = useState<AddressDetails>({
     addressLine1: "",
     addressLine2: "",
     city: "",
     stateProvince: "",
-    postalCode: null,
+    postalCode: "",
   });
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        padding: "2rem"
-      }}
-    >
+    <div className="cardContainer">
       <Card
         style={{
           width: "700px",
           padding: "1rem",
-          margin: "1rem"
+          margin: "1rem",
         }}
       >
         <Form>
           <FormGroup headerText="Employee Details">
             <FormItem labelContent={<span>Name</span>}>
-              <Input placeholder="Enter Name" value={basicDetails.name} onInput={(e) => {
-                setBasicDetails(prev => ({ ...prev, name: e.target.value }))
-              }} />
+              <Input
+                placeholder="Enter Name"
+                value={basicDetails.name}
+                onInput={(e) => {
+                  setBasicDetails((prev) => ({
+                    ...prev,
+                    name: e.target.value,
+                  }));
+                }}
+              />
             </FormItem>
 
             <FormItem labelContent={<span>Email</span>}>
-              <Input placeholder="Enter Email" value={basicDetails.email} onInput={(e) => {
-                setBasicDetails(prev => ({ ...prev, email: e.target.value }))
-              }} />
+              <Input
+                placeholder="Enter Email"
+                value={basicDetails.email}
+                onInput={(e) => {
+                  setBasicDetails((prev) => ({
+                    ...prev,
+                    email: e.target.value,
+                  }));
+                }}
+              />
             </FormItem>
 
             <FormItem labelContent={<span>Department</span>}>
               <Select
-                onChange={(e) => (e.detail.selectedOption.innerText === 'DELIVERY BOY') ? setShowAddress(false) : setShowAddress(true)}>
+                onChange={(e) =>
+                  e.detail.selectedOption.innerText === "DELIVERY BOY"
+                    ? setShowAddress(false)
+                    : setShowAddress(true)
+                }
+              >
                 <Option>CUSTOMER</Option>
                 <Option>RESTAURANT OWNER</Option>
                 <Option>DELIVERY BOY</Option>
@@ -103,37 +122,84 @@ function Register() {
             </FormItem>
 
             <FormItem labelContent={<span>Mobile Number</span>}>
-              <Input placeholder="Enter Mobile Number" value={basicDetails.number} onInput={(e) => {
-                setBasicDetails(prev => ({ ...prev, number: e.target.value }))
-              }} />
+              <Input
+                placeholder="Enter Mobile Number"
+                value={basicDetails.number}
+                onInput={(e) => {
+                  setBasicDetails((prev) => ({
+                    ...prev,
+                    number: e.target.value,
+                  }));
+                }}
+              />
             </FormItem>
           </FormGroup>
 
-          {showAddress && <AddressBox addressDetails={addressDetails} setaddressDetails={setaddressDetails}  />}
+          {showAddress && (
+            <AddressBox
+              addressDetails={addressDetails}
+              setAddressDetails={setAddressDetails}
+            />
+          )}
         </Form>
       </Card>
     </div>
   );
 }
 
-
-function AddressBox({addressDetails, setaddressDetails}) {
+function AddressBox({ addressDetails, setAddressDetails }: AddressBoxProps) {
   return (
     <FormGroup headerText="Address">
       <FormItem labelContent={<span>Address Line 1</span>}>
-        <Input placeholder="Address Line 1" value={addressDetails.addressLine1} />
+        <Input
+          placeholder="Address Line 1"
+          value={addressDetails.addressLine1}
+          onInput={(e) => {
+            setAddressDetails((prev) => ({
+              ...prev,
+              addressLine1: e.target.value,
+            }));
+          }}
+        />
       </FormItem>
 
       <FormItem labelContent={<span>Address Line 2</span>}>
-        <Input placeholder="Address Line 2" value={addressDetails.addressLine2} />
+        <Input
+          placeholder="Address Line 2"
+          value={addressDetails.addressLine2}
+          onInput={(e) => {
+            setAddressDetails((prev) => ({
+              ...prev,
+              addressLine2: e.target.value,
+            }));
+          }}
+        />
       </FormItem>
 
       <FormItem labelContent={<span>City</span>}>
-        <Input placeholder="Enter City" value={addressDetails.city} />
+        <Input
+          placeholder="Enter City"
+          value={addressDetails.city}
+          onInput={(e) => {
+            setAddressDetails((prev) => ({
+              ...prev,
+              city: e.target.value,
+            }));
+          }}
+        />
       </FormItem>
 
       <FormItem labelContent={<span>State</span>}>
-        <Input placeholder="Enter State" value={addressDetails.stateProvince} />
+        <Input
+          placeholder="Enter State"
+          value={addressDetails.stateProvince}
+          onInput={(e) => {
+            setAddressDetails((prev) => ({
+              ...prev,
+              stateProvince: e.target.value,
+            }));
+          }}
+        />
       </FormItem>
     </FormGroup>
   );
